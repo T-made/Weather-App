120b23438ae475e05a656090720c56aeeb8fb48a
T-made <56409631+T-made@users.noreply.github.com>
2020-08-11 12:34:52 -0500

    altered the weather UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Diplay the city name when user uses search engine
+//Display the city name when user uses search engine
 function searchForCity(event) {
   event.preventDefault();
   let searchCity = document.querySelector("#search-engine");
@@ -15,11 +15,53 @@ let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", searchForCity);
 
 //Display current date and time
-//Ex: Thursday 4:30
-let now = new Date();
+//Ex: Thursday 4:30 AM / PM
+function updateMin(currentMinutes) {
+  if (currentMinutes <= 10) {
+    return "0" + currentMinutes;
+  } else {
+    return currentMinutes;
+  }
+}
 
-let hours = now.getHours();
-let minutes = now.getMinutes();
+function updateTime(currentHours) {
+  if (currentHours >= 12) {
+    return "PM";
+  } else {
+    return "AM";
+  }
+}
+let now = new Date();
+let hour = [
+  12,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+];
+let time = updateTime(now.getHours());
+let min = updateMin(now.getMinutes());
+let hours = hour[now.getHours()];
+//let minutes = now.getMinutes();
 
 let daysOfWeek = [
   "Sunday",
@@ -28,25 +70,89 @@ let daysOfWeek = [
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
+  "Saturday",
 ];
 let day = daysOfWeek[now.getDay()];
 
 let p = document.querySelector("#current-time");
-p.innerHTML = `${day} ${hours}:${minutes}`;
+p.innerHTML = `${day} ${hours}:${min} ${time}`;
+
+//Change the current city using API
+function currentWeather(response) {
+  let max = Math.round(response.data.main.temp_max);
+  let min = Math.round(response.data.main.temp_min);
+
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#min-max").innerHTML = `${max}°| ${min}°`;
+  document.querySelector("#info").innerHTML =
+    response.data.weather[0].description;
+}
+function searchCity(city) {
+  let units = "imperial";
+  let apiKey = "804ddac989d26c1ff5ab45393677677e";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiURL}&appid=${apiKey}`).then(currentWeather);
+}
+
+function submission(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-engine");
+  searchCity(city.value);
+}
+let search = document.querySelector(".search-button");
+search.addEventListener("click", submission);
+
+//find location
+function updateCurrentLocation(position) {
+  let units = "imperial";
+  let apiKey = "804ddac989d26c1ff5ab45393677677e";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiURL).then(currentWeather);
+}
+function findCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(updateCurrentLocation);
+}
+
+let locationButton = document.querySelector("#current-location");
+locationButton.addEventListener("click", findCurrentLocation);
+
+searchCity("Kansas City");
 
 //Switch between Celsius and Fahrenheit
 function changeToCelsius(event) {
   event.preventDefault();
-  let link = document.querySelector("#current-temp");
-  link.innerHTML = "25°";
+  let currentTemp = document.querySelector("#current-temp");
+  let CTemp = Math.round((currentTemp.innerHTML - 32) * (5 / 9));
+  currentTemp.innerHTML = CTemp;
 }
-function changeToFahrenheit(event) {
-  let link1 = document.querySelector("#current-temp");
-  link1.innerHTML = "77°";
-}
-let changeTempC = document.querySelector("#celsius-link");
-changeTempC.addEventListener("click", changeToCelsius);
 
-let changeTempF = document.querySelector("#fahrenheit-link");
-changeTempF.addEventListener("click", changeToFahrenheit);
+function changeToFahrenheit(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#current-temp");
+  let FTemp = Math.round(currentTemp.innerHTML * (9 / 5) + 32);
+  currentTemp.innerHTML = FTemp;
+}
+let celsius = document.querySelector("#celsius-link");
+celsius.addEventListener("click", changeToCelsius);
+
+let fahrenheit = document.querySelector("#fahrenheit-link");
+fahrenheit.addEventListener("click", changeToFahrenheit);
+
+function updateTimeOfDay() {
+  let currentDay = new Date();
+  let currentHour = currentDay.getHours;
+
+  if (currentHour < 12) {
+    document.querySelector("#timeOfDay-text").innerHTML = "GOOD MORNING";
+  }
+  if (currentHour < 18) {
+    document.querySelector("#timeOfDay-text").innerHTML = "GOOD AFTERNOON";
+  } else {
+    document.querySelector("#timeOfDay-text").innerHTML = "GOOD EVENING";
+  }
+}
+updateTimeOfDay();
